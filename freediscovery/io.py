fec4ba71bc2052eb81e9e92ea59c5e3f16a3f0c1
Roman Yurchak <rth.yurchak@gmail.com>
2017-01-07 23:51:43 +0100

    Adding a pasers for SMART stemmed format

--- freediscovery/io.py
+++ freediscovery/io.py
@@ -7,6 +7,7 @@ from __future__ import unicode_literals
 
 import pandas
 import platform
+from collections import OrderedDict
 
 
 def parse_ground_truth_file(filename):
@@ -16,3 +17,29 @@ def parse_ground_truth_file(filename):
     if platform.system() == 'Windows':
         df.index = df.index.map(lambda path: path.replace('/', '\\'))
     return df
+
+
+def parse_smart_tokens(text):
+
+    res = OrderedDict()
+
+    docid = None
+    document_text = []
+
+    for line in text.splitlines():
+        if line.startswith('.I'):
+            if docid is not None:
+                res[docid] = document_text
+            document_text = []
+            _, docid = line.split(' ')
+        elif line.startswith('.W') or not line:
+            pass
+        else:
+            document_text += line.split(' ')
+
+    if document_text and docid is not None:
+        res[docid] = document_text
+
+    return res
+
+
--- freediscovery/tests/test_io.py
+++ freediscovery/tests/test_io.py
@@ -3,10 +3,11 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 import os.path
 from numpy.testing import assert_allclose
 
-from freediscovery.io import parse_ground_truth_file
+from freediscovery.io import parse_ground_truth_file, parse_smart_tokens
 
 
 def test_parse_ground_truth_file():
@@ -14,3 +15,22 @@ def test_parse_ground_truth_file():
     filename = os.path.join(basename, "..","data", "ds_001", "ground_truth_file.txt")
     res = parse_ground_truth_file(filename)
     assert_allclose(res.values[:,0] , [1, 1, 1, 0, 0, 0])
+
+
+def test_parse_smart_stemmed():
+    from textwrap import dedent
+    text = dedent("""\
+    .I 26187
+    .W
+    sunday complet tabulat race race race top top andret brazil brazil brazil rahal bobby lola lola lola cosworth cosworth cosworth cosworth ford ford ford ford mph reynard reynard reynard reynard
+    reynard reynard merced merced merced christ benz benz benz motor motor fittipald jimmy vass al uns grand prix finish finish countr chass gil fer pensk bryan hert michael adrian gordon goodyear
+    lap robby driv kph canad hond hond hond vancouv vancouv de indycar indycar scot fernandez jr
+
+    .I 26188
+    .W
+    sunday won race race andret andret rahal bobby lola lola ford ford reynard merced christ benz motor fittipald grand prix finish finish michael win vancouv vancouv indycar indycar
+    """)
+
+    res = parse_smart_tokens(text)
+    assert list(res.keys()) == ['26187', '26188']
+    assert len(res['26188']) == 28
