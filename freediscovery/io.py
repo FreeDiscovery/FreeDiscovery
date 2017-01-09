80595c027795844b35fdfcd5c5e4c139d3cdd232
Roman Yurchak <rth.yurchak@gmail.com>
2017-01-09 14:13:19 +0100

    Adding a parser for RCV1 SMART stemmed format (#49)
    * Adding a pasers for SMART stemmed format
    * Adding function documentation

--- freediscovery/io.py
+++ freediscovery/io.py
@@ -7,6 +7,7 @@ from __future__ import unicode_literals
 
 import pandas
 import platform
+from collections import OrderedDict
 
 
 def parse_ground_truth_file(filename):
@@ -16,3 +17,47 @@ def parse_ground_truth_file(filename):
     if platform.system() == 'Windows':
         df.index = df.index.map(lambda path: path.replace('/', '\\'))
     return df
+
+
+def parse_rcv1_smart_tokens(text):
+    """
+    Parse a dataset stored in the SMART tokenized format, used
+    in particular for the RCV1-v2 dataset,
+       http://www.jmlr.org/papers/volume5/lewis04a/lyrl2004_rcv1v2_README.htm
+    (cf. Appendix B.12.i.)
+
+    Parameters
+    ----------
+    text : str
+       the full text of the dataset
+
+
+    Returns
+    -------
+    result : dict
+       the parsed dataset in a OrderedDict, with document_ids as keys,
+       and a string of tokens as values
+    """
+
+    res = OrderedDict()
+
+    docid = None
+    document_text = []
+
+    for line in text.splitlines():
+        if line.startswith('.I'):
+            if docid is not None:
+                res[docid] = document_text
+            document_text = []
+            _, docid = line.split(' ')
+        elif line.startswith('.W') or not line:
+            pass
+        else:
+            document_text += line.split(' ')
+
+    if document_text and docid is not None:
+        res[docid] = document_text
+
+    return res
+
+
