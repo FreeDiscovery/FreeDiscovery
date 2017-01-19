09cacc6fd4f69cc1bd4798c72ab65fac78c5b9c4
Roman Yurchak <rth.yurchak@gmail.com>
2017-01-19 18:17:09 +0100

    Creating the index database

--- freediscovery/ingestion.py
+++ freediscovery/ingestion.py
@@ -7,6 +7,7 @@ from __future__ import unicode_literals
 
 import os.path
 import os
+import pandas as pd
 from .exceptions import (DatasetNotFound, InitException, NotFound, WrongParameter)
 
 def _list_filenames(data_dir, dir_pattern=None, file_pattern=None):
@@ -23,7 +24,7 @@ def _list_filenames(data_dir, dir_pattern=None, file_pattern=None):
     # make sure that sorting order is deterministic
     return sorted(filenames)
 
-def _prepare_data_ingestion(data_dir, metadata, file_pattern, dir_pattern):
+def _prepare_data_ingestion(data_dir, metadata, file_pattern=None, dir_pattern=None):
     """ Do some preprocessing of the input parameters
     of FeatureVectorizer for data ingestion
 
@@ -39,23 +40,45 @@ def _prepare_data_ingestion(data_dir, metadata, file_pattern, dir_pattern):
     -------
     filenames : list
       a list of filenames
-    metadata : a pandas.DataFrame
-      with at least keys 'filanames', optionally 'document_id' and 'rendition_id'
+    db : a pandas.DataFrame
+      with at least keys 'file_path', 'internal_id', optionally 'document_id' and 'rendition_id'
     """
 
-    if data_dir is not None:
+    if metadata is not None:
+        metadata = sorted(metadata, key=lambda x: x['file_path'])
+        filenames = [el['file_path'] for el in metadata]
+
+        data_dir = os.path.commonprefix(filenames)
+        data_dir = os.path.normpath(data_dir)
+
+        if not os.path.exists(data_dir) and os.path.exists(os.path.dirname(data_dir)):
+            data_dir = os.path.dirname(data_dir)
+        else:
+            raise IOError('data_dir={} does not exist!'.format(data_dir))
+
+        if not filenames: # no files were found
+            raise WrongParameter('No files to process were found!')
+        filenames_rel = [os.path.relpath(el, data_dir) for el in filenames]
+
+        # modify the metadata list inplace
+        for db_el, file_path in zip(metadata, filenames_rel):
+            db_el['file_path'] = file_path
+        db = pd.DataFrame(metadata)
+
+    elif data_dir is not None:
         data_dir = os.path.normpath(data_dir)
 
         if not os.path.exists(data_dir):
             raise NotFound('data_dir={} does not exist'.format(data_dir))
 
         filenames = _list_filenames(data_dir, dir_pattern, file_pattern)
+        filenames_rel = [os.path.relpath(el, data_dir) for el in filenames]
+        db = [{'file_path': file_path, 'internal_id': idx} \
+                            for idx, file_path in enumerate(filenames_rel)]
+
+        db = pd.DataFrame(db)
     else:
-        metadata = sorted(metadata, key=lambda x: x['file_path'])
-        filenames = [el['file_path'] for el in metadata]
-        data_dir = None
+        raise ValueError('At least one if data_dir, metadata must be provided')
 
-        if not filenames: # no files were found
-            raise WrongParameter('No files to process were found!')
 
-    return data_dir, filenames, metadata
+    return data_dir, filenames, db
--- freediscovery/tests/test_ingestion.py
+++ freediscovery/tests/test_ingestion.py
@@ -20,25 +20,30 @@ basename = os.path.dirname(__file__)
 data_dir = os.path.join(basename, "..", "data", "ds_001", "raw")
 
 
+fnames_in = ['0.7.47.101442.txt',
+             '0.7.47.117435.txt',
+             '0.7.6.28635.txt',
+             '0.7.6.28636.txt',
+             '0.7.6.28637.txt',
+             '0.7.6.28638.txt']
+fnames_in_abs = [os.path.join(data_dir, el) for el in fnames_in]
 
 def test_ingestion_base_dir():
-    data_dir_res, filenames, md = _prepare_data_ingestion(data_dir, None, None, None)
+    data_dir_res, filenames, db = _prepare_data_ingestion(data_dir, None)
     assert data_dir_res == os.path.normpath(data_dir)
-    print(filenames)
+    assert_array_equal(db.columns.values, ['file_path', 'internal_id'])
+    assert_array_equal(db.file_path.values, fnames_in)
+
 
 def test_ingestion_metadata():
-    fnames_in = ['0.7.47.101442.txt',
-          '0.7.47.117435.txt',
-          '0.7.6.28635.txt',
-          '0.7.6.28636.txt',
-          '0.7.6.28637.txt',
-          '0.7.6.28638.txt']
 
-    fnames_in = [os.path.join(data_dir, el) for el in fnames_in]
 
-    metadata = [ {'file_path': fname } for fname in fnames_in]
+    metadata = [ {'file_path': fname } for fname in fnames_in_abs]
 
-    data_dir_res, filenames, md = _prepare_data_ingestion(data_dir, None, None, None)
+    data_dir_res, filenames, md_table = _prepare_data_ingestion(None, metadata)
+
+    assert data_dir_res == os.path.normpath(data_dir)
+    assert filenames == fnames_in_abs
 
 
 
