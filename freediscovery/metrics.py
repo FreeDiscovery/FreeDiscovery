01db65eb64c9f3f45fb163687ac355b7220f883b
Roman Yurchak <rth.yurchak@gmail.com>
2017-07-07 14:46:48 +0300

    Backported sklearn metrics from v0.19

--- freediscovery/metrics.py
+++ freediscovery/metrics.py
@@ -227,8 +227,8 @@ def categorization_score(idx_ref, Y_ref, idx, Y):
     # An equivalent functionally should be achieved with a
     # more general freediscovery.metrics module
     import warnings
-    from sklearn.metrics import (precision_score, recall_score, f1_score,
-            roc_auc_score, average_precision_score)
+    from sklearn.metrics import precision_score, recall_score, f1_score
+    from freediscovery.externals.sklearn_backport.metrics import roc_auc_score, average_precision_score
     threshold = 0.0
 
     idx = np.asarray(idx, dtype='int')
